cfbae5ca0bbf08074adb6e656c8d04b104bda2d0
Chin <chin.bimbo@gmail.com>
2014-10-26 16:26:06 -0400

    update pvp

--- bb_pvp/js/helper.js
+++ bb_pvp/js/helper.js
@@ -11,21 +11,21 @@ function setPreviousChoices() {
 
     // player 2 fams
     if (localStorage.getItem("f10") && localStorage.getItem("f10") != "null") {
-        for (var i = 0; i < 10; i++) {
+        for (i = 0; i < 10; i++) {
             document.getElementById("f" + (i + 10)).value = localStorage.getItem("f" + (i + 10));
         }
     }
 
     // player 1 skills
     if (localStorage.getItem("s10") && localStorage.getItem("s10") != "null") {
-        for (var i = 0; i < 3; i++) {
+        for (i = 0; i < 3; i++) {
             document.getElementById("s1" + i).value = localStorage.getItem("s1" + i);
         }
     }
 
     // player 2 skills
     if (localStorage.getItem("s20") && localStorage.getItem("s20") != "null") {
-        for (var i = 0; i < 3; i++) {
+        for (i = 0; i < 3; i++) {
             document.getElementById("s2" + i).value = localStorage.getItem("s2" + i);
         }
     }
@@ -149,7 +149,7 @@ function setFamOptions() {
 
     for (var i = 0; i < famSelects.length; i++) {
         for (var index = 0; index < famIdArray.length; index++) {
-            var key = famIdArray[index];
+            key = famIdArray[index];
             var option = document.createElement("option");
             option.value = key;
             option.text = famDatabase[key].fullName;
@@ -226,7 +226,7 @@ function getBattleDataOption() {
         if (!option.p1RandomMode) localStorage.setItem("f" + i, f1id);
         if (!option.p2RandomMode) localStorage.setItem("f" + (i + 10), f2id);
     }
-    for (var i = 0; i < 3; i++) {
+    for (i = 0; i < 3; i++) {
         var w1s = getURLParameter("s1" + i);
         var w2s = getURLParameter("s2" + i);
         data.p1_warlordSkillIds.push(w1s);
@@ -249,29 +249,77 @@ function prepareRandom() {
     }
 }
 
-function battleFinishedCallback() {
+function onBattleFinished() {
     document.getElementById("startButton").disabled = false;
-    setTimeout(function () {
-        if (!localStorage.getItem("starRequestShown")) {
-            showStarRequest();
-        }
-    }, 2000);
+    showStarRequest();
+}
 
+function onSimulationFinished() {
+    showStarRequest();
 }
 
 function showStarRequest() {
-    swal({
-        title: "Star this!",
-        text: "If you like this simulator, star the project on Github. It motivates me to improve it :)",
-        type: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#5cb85c",
-        confirmButtonText: "Take me there",
-        closeOnConfirm: false
-    }, function () {
-        localStorage.setItem("starRequestShown", true);
-        window.location.href = 'https://github.com/chinhodado/Blood-Brothers-PvP-Simulator';
-    });
+    setTimeout(function () {
+        if (!localStorage.getItem("starRequestShown")) {
+            swal({
+                title: "Star this!",
+                text: "If you like this simulator, star the project on Github. It motivates me to improve it :)",
+                type: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#5cb85c",
+                confirmButtonText: "Take me there",
+                closeOnConfirm: false
+            }, function () {
+                localStorage.setItem("starRequestShown", true);
+                window.location.href = 'https://github.com/chinhodado/Blood-Brothers-PvP-Simulator';
+            });
+        }
+    }, 2000);
+}
+
+function getRandomBackground() {
+    var bg = [
+        "23b/Bamboo01", "34d/Bamboo02", "1c5/Carpet01", "141/Carpet02",
+        "283/Carpet03", "1f8/Carpet04", "193/Carpet05", "24d/Carpet06",
+        "17b/Carpet07", "3ff/Carpet08", "1e6/Carpet09", "3c3/Carpet10",
+        "3a3/Carpet11", "224/Carpet12", "4ad/Carpet13", "20c/Carpet14",
+        "29f/Carpet15", "21c/Carpet16", "385/Carpet17", "4f8/Carpet18",
+        "362/Carpet19", "387/Carpet20", "311/Carpet21", "352/Carpet22",
+        "392/Castle01", "2f9/Castle02", "3b4/Cave01", "266/Cave02",
+        "3bc/Cave03", "1ad/Cave04", "4d5/Cave05", "3bf/Desert01",
+        "4c9/Desert02", "3d9/Fog01", "30e/Fog02", "267/Forest01",
+        "2c5/Forest02", "247/Greatwall01", "450/Halloween01", "22e/Halloween02",
+        "28a/Halloween03", "11a/Jungle01", "268/Mountain01", "3fb/River01",
+        "451/River02", "49f/Road01", "270/Road02", "475/Road03",
+        "2a8/Road04", "40c/Road05", "2ff/Road06", "310/Road07",
+        "383/Road08", "41e/Road09", "289/Road10", "183/Road11",
+        "1d8/Road12", "2a7/Road13", "3cf/Road14", "3fb/Road15",
+        "1f4/Road16", "28f/Road17", "2a5/Road28", "102/Road29",
+        "4e9/Ruins01", "1f4/Sakura01", "336/Snow01", "49a/Swamp01",
+        "145/Swamp02", "144/Tints01", "1fb/Tree01", "33c/Tree02",
+        "329/81a5ccfd07ca41c238e124a5b6683b93", "1a0/Castle1", "39f/F459e81069786396191c375060d778a3",
+        "3b1/66fddb4d129fa8b494cf3d21a057e226", "45f/452d87b11eb533d33fba937073bb5668",
+        "4a5/48645b3ae0106d4f96fa0bf3ad6239b8"
+    ];
+
+    var shortenedLink = getRandomElement(bg);
+    var firstPart = "http://img" + shortenedLink.charAt(0) + ".wikia.nocookie.net/bloodbrothersgame/images/";
+    var link = firstPart + shortenedLink.charAt(1) + "/" + shortenedLink.substring(1) + ".png";
+    return link;
+}
+
+function prepareField() {
+    var rndBgLink = getRandomBackground();
+    var img = new Image();
+    var svgWrapper = document.getElementById("svgWrapper");
+    img.onload = function() {
+        svgWrapper.style.backgroundImage = "url('" + rndBgLink + "')";
+    };
+    img.onerror = function() {
+        console.log("Background not found: " + rndBgLink);
+        svgWrapper.style.backgroundImage = "url(img/bg01.png)";
+    };
+    img.src = rndBgLink;
 }
 
 // fetch the tier list and cache it
